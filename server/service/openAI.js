c36d3179529afdcaef995c98fe4431afaaf07f90
Naman Raina <namanr2@illinois.edu>
2024-02-19 16:06:22 -0600

    fixing the axios error

--- server/service/openAI.js
+++ server/service/openAI.js
@@ -232,7 +232,7 @@ export const generateCompletion = async (messages) => {
     if (messages.filter((m) => m.sender===2).length > 0) {
         let tryTimes = 0;
         do {
-            const restext = await httpGPTCompletion("gpt-4", prompt, 0.7);
+            const restext = await httpGPTCompletion("gpt-3.5", prompt, 0.7);
             const res = {text: restext};
             res.text = filterContent(messages, res.text);
             const i = checkRepeat(setArray, res.text);
@@ -249,7 +249,7 @@ export const generateCompletion = async (messages) => {
         let tryTimes = 0;
         const insForAI = `${AI_INS} ${messages[0].text}. ${prompt}`;
         do {
-            const restext = await httpGPTCompletion("gpt-4", insForAI, 0.7);
+            const restext = await httpGPTCompletion("gpt-3.5", insForAI, 0.7);
             const res = {text: restext};
             res.text = filterContent(messages, res.text);
             const i = checkRepeat(setArray, res.text);
