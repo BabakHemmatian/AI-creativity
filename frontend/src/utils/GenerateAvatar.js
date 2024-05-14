29649e59fcffa80527b51e55040f1cbffa1ebecb
Naman Raina <namanr2@illinois.edu>
2024-05-14 18:48:26 -0500

    Modified GenerateAvatar.js APIs

--- frontend/src/utils/GenerateAvatar.js
+++ frontend/src/utils/GenerateAvatar.js
@@ -1,11 +1,11 @@
 const generateDiceBearAvataaars = (seed) =>
-  `https://avatars.dicebear.com/api/avataaars/${seed}.svg`;
+  `https://api.dicebear.com/8.x/avataaars/svg?seed=${seed}`;
 
 const generateDiceBearBottts = (seed) =>
-  `https://avatars.dicebear.com/api/bottts/${seed}.svg`;
+  `https://api.dicebear.com/8.x/bottts/svg?seed=${seed}`;
 
 const generateDiceBearGridy = (seed) =>
-  `https://avatars.dicebear.com/api/gridy/${seed}.svg`;
+  `https://api.dicebear.com/8.x/open-peeps/svg?seed=${seed}`;
 
 export const generateAvatar = () => {
   const data = [];
