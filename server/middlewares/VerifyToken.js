bd362fa9a9c9dca1eceaab4c4044196ad6fc9222
Naman Raina <namanr2@illinois.edu>
2024-05-17 15:42:33 -0500

    trying to add haotian's suggestions

--- server/middlewares/VerifyToken.js
+++ server/middlewares/VerifyToken.js
@@ -5,33 +5,58 @@
 import auth from "../config/firebase-config.js";
 
 export const VerifyToken = async (req, res, next) => {
-  //console.log("Request Object:", req);
-  //console.log("Request Headers:", req.headers);
-  const token = req.headers.authorization.split(" ")[1];
-
-  try {
-    const decodeValue = await auth.verifyIdToken(token);
-    if (decodeValue) {
-      req.user = decodeValue;
-      return next();
+  const Authorization_Header = req.headers.authorization;
+  if (Authorization_Header)
+    {  
+    const token = Authorization_Header.split(" ")[1];
+    try {
+      const decodeValue = await auth.verifyIdToken(token);
+      if (decodeValue) {
+        req.user = decodeValue;
+        return next();
+      }
+      else 
+      {
+        console.log("Unauthorized - VerifyToken function");
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+    } catch (e) {
+      console.log("Internal Error - VerifyToken function");
+      return res.status(500).json({ message: "Internal Error" });
     }
-  } catch (e) {
-    return res.json({ message: "Internal Error" });
+  } 
+  else 
+  {
+    console.log("No Authorization Header - VerifyToken function");
+    return res.status(401).json({ message: "No Authorization Header!" });
   }
 };
 
 export const VerifySocketToken = async (socket, next) => {
-  const token = socket.handshake.auth.token;
+  const handshake_auth = socket.handshake.auth;
 
-  try {
-    const decodeValue = await auth.verifyIdToken(token);
-
-    if (decodeValue) {
-      socket.user = decodeValue;
-
-      return next();
+  if (handshake_auth)
+  {
+    const token = socket.handshake.auth.token;
+    try {
+      const decodeValue = await auth.verifyIdToken(token);
+      if (decodeValue) 
+      {
+        socket.user = decodeValue;
+        return next();
+      }
+      else 
+      {
+        console.log("Unauthorized - VerifySocketToken function");
+        return next(new Error("Unauthorized"));
+      }
+    } catch (e) {
+      console.log("Internal Error - VerifySocketToken function");
+      return next(new Error("Internal Error"));
     }
-  } catch (e) {
-    return next(new Error("Internal Error"));
+  }else
+  {
+    console.log("No Authorization Header - VerifySocketToken function");
+    return next(new Error("No Authorization Header"));
   }
 };
