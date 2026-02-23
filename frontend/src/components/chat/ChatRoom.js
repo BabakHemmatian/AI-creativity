3facc90f05d8bd31941ddf88d5475f956325a740
Babak Hemmatian <babak.hemmatian@gmail.com>
2026-02-23 16:37:31 -0600

    Update placeholder text in ChatRoom component

--- frontend/src/components/chat/ChatRoom.js
+++ frontend/src/components/chat/ChatRoom.js
@@ -289,7 +289,7 @@ export default function ChatRoom({
           <textarea
             value={scratchpad}
             onChange={(e) => setScratchpad(e.target.value)}
-            placeholder="Jot down ideas here if waiting for your partner to respond..."
+            placeholder="You can organize ideas here..."
             className="w-full p-1 text-sm rounded-md dark:bg-gray-800 dark:text-gray-300"
             rows={4}
           />
